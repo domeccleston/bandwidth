f6e11fb67b52b70e8a48e395f9dbc0e666d97f25
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2023-09-21 11:30:22 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,76 +15,9 @@ type BandwidthForm = {
 };
 
 const Home: NextPage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<BandwidthForm>();
-
-  const [visitors, setVisitors] = useState<null | number>(null);
-  const [pageWeight, setPageWeight] = useState("");
-  const [bandwidthEstimate, setBandwidthEstimate] = useState<null | number>(
-    null
-  );
-
-  const onSubmit = async ({ siteUrl, monthlyVisitors }: BandwidthForm) => {
-    setVisitors(monthlyVisitors);
-    const data = await (await fetch(`/api/pageweight/?url=${siteUrl}`)).json();
-    setPageWeight(data);
-    setBandwidthEstimate(Number(data) * monthlyVisitors);
-  };
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Bandwidth Estimator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Monthly bandwidth estimator</h1>
-        <h4 className={styles.subtitle}>
-          Do you have a customer who wants to know how much bandwidth their website will consume
-          on Vercel? This can help give an initial estimate.
-        </h4>
-        <form
-          className={styles.bandwidthForm}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Input
-            className={styles.input}
-            {...register("monthlyVisitors", {
-              required: true,
-              pattern: /^[1-9]\d*$/,
-            })}
-            placeholder="Unique monthly visitors"
-            type={errors.monthlyVisitors ? "error" : "default"}
-          />
-          <Input
-            className={styles.input}
-            {...register("siteUrl", {
-              required: true,
-              pattern:
-                /^(?:(ftp|http|https)?:\/\/)?(?:[\w-]+\.)+([a-z]|[A-Z]|[0-9]){2,6}$/i,
-            })}
-            placeholder="https://yourwebsite.co"
-            type={errors.siteUrl ? "error" : "default"}
-          />
-          <Button
-            htmlType="submit"
-            loading={isSubmitting}
-            type="success"
-            className={styles.button}
-          >
-            Submit
-          </Button>
-        </form>
-        {bandwidthEstimate && (
-          <h3 className={styles.estimate}>
-            Estimated bandwidth usage: {formatted(bandwidthEstimate)}.
-          </h3>
-        )}
-      </main>
+      <h1>Deprecated – for bandwidth estimations please contact @dom eccleston on Slack</h1>
     </div>
   );
 };
